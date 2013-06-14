2300ce40f04a9e84f60557ae937b9a2d994d3d89
duanhongyi <duanhongyi@sunmap.com>
2013-06-14 16:35:37 +0800

    添加通用views

---
+++ django_swift/views.py
@@ -0,0 +1,20 @@
+import logging
+from django.core.files.storage import default_storage
+from django.http.response import StreamingHttpResponse, HttpResponse
+
+
+logger = logging.getLogger()
+
+def download(request, name):
+    try:
+        f = default_storage.open(name)
+        return StreamingHttpResponse(iter(f))
+    except BaseException as e:
+        response = HttpResponse()   
+        if hasattr('http_status', e):
+            response.status_code = e.http_status
+        else:
+            response.status_code = 500
+            logger.exception(e)
+    return response
+    
