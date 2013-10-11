1ba107bfc2db9ab665cba2fff9881f37ed467148
duanhongyi <duanhongyi@sunmap.com>
2013-10-11 10:02:34 +0800

    add request header support

--- django_swift/views.py
+++ django_swift/views.py
@@ -7,11 +7,15 @@ logger = logging.getLogger()
 
 def download(request, name):
     try:
+        request_headers = {}
+        for key, value request.META.viewitems():
+            request_headers[key.replace('_', '-')] = value
         f = default_storage.open(name)
         headers, data = f.connection.get_object(
             f.container_name,
             f.name,
-            resp_chunk_size = 1024 * 1024
+            resp_chunk_size = 1024 * 1024,
+            headers = request_headers
         )
         response = StreamingHttpResponse(data)
         for item in headers.viewitems():
